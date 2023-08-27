c81ac9a48cf7703e5020a04f0d64e8931191613f
Mohitb07 <bmohit980@gmail.com>
2023-08-27 20:26:36 +0530

    perf: components restructured and code refactored

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
-import DiscoveryFacility from "../components/DiscoveryFacility";
-import DiscoveryMethod from "../components/DiscoveryMethod";
-import DiscoveryYear from "../components/DiscoveryYear";
-import HostnameDropdown from "../components/Hostname";
-import Table from "../components/Table";
-import { usePlanetDataContext } from "../hooks/useData";
-import { useSearch } from "../hooks/useSearch";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 
-// flex flex-col md:flex-wrap md:flex-row lg:flex-nowrap
+import ClearButton from "../components/Buttons/ClearButton";
+import SearchButton from "../components/Buttons/SearchButton";
+import DropdownForms from "../components/DropdownForms";
+import Table from "../components/Table";
+import { useSearch } from "../hooks/useSearch";
 
 const Homepage = () => {
-  const { searchQuery } = usePlanetDataContext();
   const {
     searchResult,
     isSearching,
@@ -19,31 +15,15 @@ const Homepage = () => {
     handleIncreasingOrder,
     handleFormReset,
   } = useSearch();
-  console.log("homepage render");
-  const handleSearchClick = () => {
-    if (searchQuery) {
-      handleSearch(searchQuery);
-    }
-  };
 
   return (
     <div className="flex h-screen justify-center">
       <div className="xl:container flex flex-col w-full md:pt-10 pt-4 space-y-5">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 px-4 items-center">
-          <HostnameDropdown />
-          <DiscoveryMethod />
-          <DiscoveryYear />
-          <DiscoveryFacility />
+          <DropdownForms />
           <div className="gap-2 md:col-span-2 md:gap-4 lg:col-span-1 grid md:grid-cols-2 lg:grid-cols-2 lg:gap-2">
-            <button
-              onClick={handleSearchClick}
-              className="bg-blue-700 px-5 rounded-xl py-2 w-full text-white"
-            >
-              Search
-            </button>
-            <button onClick={handleFormReset} className="bg-blue-700 px-5 rounded-xl py-2 w-full text-white">
-              Clear
-            </button>
+            <SearchButton onClick={handleSearch} />
+            <ClearButton onClick={handleFormReset} />
           </div>
         </div>
         <div className="h-full w-full">
@@ -55,7 +35,7 @@ const Homepage = () => {
           />
         </div>
       </div>
-      <Toaster position="bottom-center"/>
+      <Toaster position="bottom-center" />
     </div>
   );
 };
