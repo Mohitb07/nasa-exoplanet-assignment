b51b8614776f80e2b97febc0664fb0594b8f9f3d
Mohitb07 <bmohit980@gmail.com>
2023-08-27 20:25:03 +0530

    refactor: new types folder for shared typescript types

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type Planet = [string, string, string, string, string];
+
+export type SearchQuery = {
+  hostname: string;
+  discoveryMethod: string;
+  discoveryYear: string;
+  discoveryFacility: string;
+};
